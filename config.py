833ea104da8a1f1fd0674632502cd0b5e2c2f011
RPi <pi@localhost>
2019-11-01 17:51:28 +0000

    Refactory

--- config.py
+++ config.py
@@ -1,5 +1,10 @@
 import yaml
+import pickle
 import numpy as np
+import cv2
+import matplotlib as mpl
+mpl.use('Agg')
+import matplotlib.pyplot as plt
 from picamera.array import PiYUVArray, PiRGBArray
 from picamera import PiCamera
 from pwm import PWM
@@ -25,6 +30,7 @@ Ki = param['pid']['ki']
 Kd = param['pid']['kd']
 
 # Camera settings
+RESOLUTION = tuple(param['camera']['resolution'])
 FRAMERATE = param['camera']['framerate']
 
 # Vision params
@@ -33,16 +39,27 @@ SCAN_LINE = param['vision']['scaneLine']
 PEAK_THRES = param['vision']['peakThres']
 TRACK_WIDTH = param['vision']['trackWidth']
 
+UNDISTORT_ENABLE = param['vision']['undistort']['enable']
+K = np.array(param['vision']['undistort']['K'])
+D = np.array(param['vision']['undistort']['D'])
+PARAM_FILE = param['vision']['undistort']['paramFile']
+with open(PARAM_FILE, "rb") as f:
+    MAP1, MAP2 = pickle.load(f)
+
 # Motor params
 SPEED = param['motor']['speed']
 
 # Run timer
 RUN_TIMER = param['run']['time']
 
+# Log setting
+LOG_FRAME = param['logging']['frame']
+LOG_CYCLE_TIME = param['logging']['cycleTime']
+LOG_SCAN_LINE = param['logging']['scanLine']
 
 ### Initialize components
 # Camera
-res = (640, 480)
+res = RESOLUTION
 camera = PiCamera()
 camera.sensor_mode = 7
 camera.resolution = res
@@ -62,7 +79,7 @@ servo.enable = True
 
 # Enable motor
 MOTOR_BRAKE = 1000000
-DECREASE_MAX = SPEED - 100000
+DECREASE_MAX = SPEED - 150000
 
 motor = PWM(0)
 motor.period = 20000000
@@ -91,12 +108,20 @@ def decreaseSpeedDuty(DUTY_CYCLE):
     return int(DECREASE_MAX * abs(DUTY_CYCLE - SERVO_MIDDLE) / SERVO_MIDDLE)
 
 # Get line_pos
-def findLinePos(I):
+def findLinePos(I, line_pos=None, undistort_enable=False, scan_hist=None):
+    # Undistort image
+    if undistort_enable:
+        I = undistort(I)
+    
     # Select a horizontal line in the middle of the image
     L = I[SCAN_LINE, :]
 
     # Smooth the transitions so we can detect the peaks 
     Lf = filtfilt(b, a, L)
+    
+    # Log scan line
+    if scan_hist != None:
+        scan_hist.append(Lf)
 
     # Find peaks which are higher than 0.5
     p = find_peaks(Lf, height=PEAK_THRES)
@@ -124,12 +149,37 @@ def findLinePos(I):
         line_pos   = line_right - TRACK_WIDTH // 2
         
     else:
-        line_pos = CAMERA_CENTER
+        if line_pos == None:
+            line_pos = CAMERA_CENTER
         print(">>>> no line <<<<")
         print("Current pos = ", line_pos, ", peaks = ", peaks)
     
     line_pos = line_pos - CAMERA_CENTER
     
     return line_pos
+
+# Undistor
+DIM = RESOLUTION
+def undistort(img, balance=0.0, dim2=None, dim3=None):
+    undistorted_img = cv2.remap(img, MAP1, MAP2, interpolation=cv2.INTER_LINEAR, borderMode=cv2.BORDER_CONSTANT)
+    return undistorted_img
+
+# Save image list
+def saveFrameList(lst, di):
+    for i, f in enumerate(lst):
+        fig = plt.figure()
+        if UNDISTORT_ENABLE:
+            f = undistort(f)
+        plt.imshow(f)
+        plt.hlines(SCAN_LINE, 0, f.shape[1], 'w')
+        fig.savefig(f"{di}/{i}.png")
+
+# Plot histogram of cycle time
+def plotCycleTime(t_hist, di, framerate, fname="cycle_time"):
+    t = np.gradient(np.array(t_hist))
+    fig = plt.figure()
+    plt.hist(t, bins=10)
+    plt.title(f"framerate = {framerate}, min_rate = {1/t.max():.2f}, max_rate = {1/t.min():.2f}")
+    fig.savefig(f"{di}/{fname}.png")
 
 print("Config has been loaded!")
--- run.py
+++ run.py
@@ -20,15 +20,37 @@ motor.duty_cycle = MOTOR_BRAKE + SPEED
 line_pos    = CAMERA_CENTER
 first_frame = True
 
+# Logging
+if LOG_FRAME:
+    frame_hist = []
+if LOG_CYCLE_TIME:
+    t_hist = []
+if LOG_SCAN_LINE:
+    scan_hist = []
+
+# Loop starting time
+t_now = time.time()
+if LOG_CYCLE_TIME:
+    t_hist.append(t_now)
+
+line_pos = None
 for f in stream:
     if first_frame:
         first_frame = False
         # Reset the buffer for the next image
         rawCapture.truncate(0)
+        # Save first frame
+        if LOG_FRAME:
+            frame_hist.append(f.array)
+            frame_hist.append(f.rgb_array)
         continue
     
     # Stop after RUN_TIMER seconds
-    if (time.time() - t) > RUN_TIMER:
+    if (t_now - t) > RUN_TIMER:
+        # Save last frame
+        if LOG_FRAME:
+            frame_hist.append(f.array)
+            frame_hist.append(f.rgb_array)
         break
     
     # Get the intensity component of the image (a trick to get black and white images)
@@ -37,14 +59,23 @@ for f in stream:
     # Reset the buffer for the next image
     rawCapture.truncate(0)
     
-    line_pos = findLinePos(I)
+    if LOG_SCAN_LINE:
+        line_pos = findLinePos(I, line_pos, undistort_enable=UNDISTORT_ENABLE, scan_hist=scan_hist)
+    else:
+        line_pos = findLinePos(I, line_pos, undistort_enable=UNDISTORT_ENABLE)
     
     # Update PID
     pid.update(line_pos)
     DUTY_CYCLE = servoLimit(SERVO_MIDDLE + int(pid.output)) 
-        
+    
+    # Update servo and motor
     servo.duty_cycle = DUTY_CYCLE
     motor.duty_cycle = (MOTOR_BRAKE + SPEED) - decreaseSpeedPos(line_pos)
+    
+    # Time logging
+    t_now = time.time()
+    if LOG_CYCLE_TIME:
+        t_hist.append(t_now)
 
 ### Endloop ###
 
@@ -56,3 +87,11 @@ servo.duty_cycle = SERVO_MIDDLE
 stream.close()
 rawCapture.close()
 camera.close()
+
+# Post process
+if LOG_FRAME:
+    saveFrameList(frame_hist, "photo")
+if LOG_CYCLE_TIME:
+    plotCycleTime(t_hist, "plot", FRAMERATE, fname="cycle_time")
+if LOG_SCAN_LINE:
+    np.save('scan_hist.npy', np.array(scan_hist))
